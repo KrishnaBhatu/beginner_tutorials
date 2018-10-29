76b711ba4184fb88ea3ad824d299f6ed173f7e96
KrishnaBhatu <kbhatu@terpmail.umd.edu>
2018-10-29 17:07:46 -0400

    Remove the catkin work directory files

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kbhatu/Desktop/ROS Publisher and Subscriber Assignment/beginner_tutorials/catkin_ws/src
-# Build directory: /home/kbhatu/Desktop/ROS Publisher and Subscriber Assignment/beginner_tutorials/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
