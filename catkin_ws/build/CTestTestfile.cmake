ee990f66e8088eb14db2f2b3d01a19a7f6e3438e
KrishnaBhatu <kbhatu@terpmail.umd.edu>
2018-10-29 13:00:57 -0400

    Make a catkin workspace which will be the working directory where all the packages will be built

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kbhatu/Desktop/ROS Publisher and Subscriber Assignment/beginner_tutorials/catkin_ws/src
+# Build directory: /home/kbhatu/Desktop/ROS Publisher and Subscriber Assignment/beginner_tutorials/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/kbhatu/Desktop/ROS Publisher and Subscriber Assignment/beginner_tutorials/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/kbhatu/Desktop/ros_test/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/kbhatu/Desktop/ros_test/catkin_ws/devel;/opt/ros/kinetic" != "" else []
