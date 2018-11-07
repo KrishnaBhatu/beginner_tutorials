3e7ef79e566c03ef7c19c62268f8c7070c2a9eac
KrishnaBhatu <kbhatu@terpmail.umd.edu>
2018-11-06 21:44:23 -0500

    Modify the Publisher source file to add a service which changes the publishing string as per the string provided by the service caller

--- src/stringPublisherNode.cpp
+++ src/stringPublisherNode.cpp
@@ -19,29 +19,61 @@
 
   * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
   * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+  * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  * POSSIBILITY OF SUCH DAMAGE.
   *  @copyright (c) BSD
   *  @file    stringPublisherNode.cpp
   *  @author  Krishna Bhatu
   *
-  *  @brief Publisher Node;
+  *  @brief Publisher Node with a service;
   *
   *  @section DESCRIPTION
   *
   *  Source code for publisher node that publishes to the /chatter topic
-  *  and inputs that string message.
+  *  and inputs that string message and it also has a service to change string.
   */
+#include <sstream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include <sstream>
- 
+#include "beginner_tutorials/changeString.h"
+
+/**
+  * This is a global string that is modified.
+  */ 
+extern std::string str = "Hi This is Krishna";
+/**
+ * @brief A change of string function which is called when a service is called 
+ *        to change the publishing message
+ * @param req It is the reference to the argument which the client
+ *            requests the service which is the used as new publish message
+ * @param resp It is the reference to the string that the service replies   
+ * @return bool It returns true is the service is called successfully
+ */
+
+bool changeString ( beginner_tutorials::changeString::Request &req ,
+		    beginner_tutorials::changeString::Response &resp){
+	resp.output = str;
+	/**
+	  * Condition check for checking if the string passed to the service is 
+	  * same as the current publishing string
+	  */
+	if(resp.output == req.a){
+		ROS_ERROR_STREAM("Change in String Not Achieved");
+	}
+	else{
+		str = req.a;
+		ROS_WARN_STREAM("The String is Modified"); 
+	}
+	return true;
+}
+
 int main(int argc, char **argv)
 {
 /**
@@ -51,9 +83,39 @@ int main(int argc, char **argv)
   */
   ros::init(argc, argv, "talker");
 /**
+  * Check if the node is successfully initialized
+  */  
+  if(ros::isInitialized ()){
+  int frequencyDefault = 100;
+  int frequency;
+/**
+  * Check for the command line argument for the frequency parameter
+  */  
+  if (argc == 2) {
+    if (argv[1] == 0){
+    	ROS_WARN_STREAM("Frequency changed to non-zero value");
+    	int frequency = frequencyDefault;
+    }
+    else{
+      /**
+        * frequency parameter is assigned with the argument value
+        */
+    	int frequency = atoi(argv[1]);
+    }
+  }
+  else{
+    frequency = frequencyDefault;
+  }
+/**
   * This fully initializes the node.
   */  
   ros::NodeHandle n;
+/**
+  * Creating a service where the first argument of advertiseService() is the 
+  * service name and the second argument is function name which defines the
+  * service 
+  */  
+  ros::ServiceServer server  = n.advertiseService("changeString" ,changeString);
 /**
   * The advertise() function defines the role of node to the ROS master.
   * The first parameter is the topic on which the node publishes.
@@ -63,12 +125,15 @@ int main(int argc, char **argv)
 /**
   * Frequency at which it loops (10Hz)
   */  
-  ros::Rate loop_rate(10);
+  ros::Rate loop_rate(frequency);
+/**
+  * LOG message to monitor the frequency of looping
+  */  
+  ROS_DEBUG_STREAM("Frequency Check: %d" ;frequency);
   while (ros::ok())
   {
      /// Message object that has data to publish
      std_msgs::String msg;
-     std::string str = "Hi This is Krishna";
      msg.data = str;
      /**
       *  The publish() function is used to send messages.
@@ -80,6 +145,10 @@ int main(int argc, char **argv)
       *  rate.
       */
      loop_rate.sleep();
-   }
+  }
+  }
+  else{
+	ROS_FATAL_STREAM("ROS is not initialized.");
+  }
   return 0;
  }
